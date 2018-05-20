1ac5814b778774a267cbd5d0bb6dd79695c2a393
Matt Trussell <smtrussell@gmail.com>
2018-05-20 17:20:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-
+    @user = User.create(name: params["name"], email: params["email"], password: params["password"])
+    session[:id] = @user.id
+    puts @user
     redirect '/users/home'
   end
 
@@ -29,7 +31,7 @@ class ApplicationController < Sinatra::Base
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
 
     redirect '/'
   end
