11d82deee4e081b49a13df6e391d557ccad3bbde
Matt Trussell <smtrussell@gmail.com>
2018-05-20 17:23:52 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2018_05_20_073632) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
 end
